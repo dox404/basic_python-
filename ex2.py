4e7dacd566b488d555c317f489593ff20ba92fa7
dox404 <79058646+dox404@users.noreply.github.com>
2021-03-14 15:54:33 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# name=input("enter your name: ")
+# print(f"the reverse of your name is {name[-1::-1]}") or,
+
+
+
+
+
+name = input("enter your name: ")
+print(name[-1::-1])
---
+++ exersize1.py
@@ -0,0 +1,9 @@
+number1=input('enter your first number ')
+number2=input('enter your second number ')
+number3=input('enter your third number ')
+avarage=(int(number1)+int(number2)+int(number3))/3
+print(f"the avarage of these three number is  {avarage}")
+  
+  
+  
+  
