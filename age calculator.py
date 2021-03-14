2dcd16edfdc47e76d1d007c0f932c42d2b97720e
dox404 <79058646+dox404@users.noreply.github.com>
2021-03-14 15:59:29 +0530

    Add files via upload

---
+++ age calculator.py
@@ -0,0 +1,11 @@
+print("Hello, this is my first ever python project . i have started with the basics....hope u like it")
+name=input("enter your name ")
+print("hello "+ name +",i am mani and i have created an age calculating system,in bottom please enter your birth year")
+
+birth_year=input("Enter your birth year ")
+
+print("Yeh boy !!! your birth year is " +birth_year+".thats great and your age is ")
+age=2021-int(birth_year)
+print(age)
+
+
